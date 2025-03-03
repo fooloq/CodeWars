bad488968dcb6936b5e24ada8703bd07e2ba4cd6
juraj <8475494@stud.kai.edu.ua>
2025-03-03 10:27:59 +0200

    Add project files.

---
+++ KataLibaryTest/KataTest.cs
@@ -0,0 +1,16 @@
+namespace KataLibaryTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
